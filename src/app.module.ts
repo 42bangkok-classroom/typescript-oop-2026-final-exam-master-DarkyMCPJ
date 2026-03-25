6f54ad386fcf7bf7961e254d0addef456326a63e
event <event@z3t2c1.42bangkok.com>
2026-03-25 10:25:55 +0700

    Meh

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { APP_FILTER } from '@nestjs/core';
 import { ApiExceptionFilter } from './common/api-exception.filter';
 
 @Module({
-  imports: [ProductModule, PurchaseModule],
+  imports: [],
   controllers: [AppController],
   providers: [
     AppService,
