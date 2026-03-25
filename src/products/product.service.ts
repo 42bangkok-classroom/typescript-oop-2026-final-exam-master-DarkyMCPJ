2207777300fcc7be28da01b286ad682acfc90a5c
event <event@z3t2c1.42bangkok.com>
2026-03-25 11:22:44 +0700

    ASK REAL

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -2,13 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { Product } from './product.interface';
 import { ApiResponse } from '../interfaces/response.interface';
 import * as fs from 'fs';
-
 @Injectable()
 export class ProductService {
   findAll(): ApiResponse<Product[]> {
     const readProductData = fs.readFileSync('data/products.json', 'utf-8');
     const productData = JSON.parse(readProductData) as Product[];
     return productData;
-    };
+  }
   }
 
